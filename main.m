c4a342247a523e67842092dd673f4032d48b1384
absidue <48293849+absidue@users.noreply.github.com>
2020-12-13 00:32:27 +0100

    Cleanup argument parsing and improve help message

--- main.m
+++ main.m
@@ -28,6 +28,7 @@ void setVolume(float volume) {
 void help(const char *path) {
     printf("\nUsage: %s action\n\n"
             "Available actions:\n"
+            "Note: Not all apps support all actions!\n"
             " playpause, toggle   play if paused/pause if playing\n"
             " play                play\n"
             " pause               pause\n"
@@ -42,6 +43,7 @@ void help(const char *path) {
             "Getting information:\n"
             " nowplaying, np      show now playing information\n"
             " artworkuri, awuri   get the artwork as a data URI\n"
+            "Help:\n"
             " help, -h, --help    show this message\n\n"
             , path);
 }
@@ -86,115 +88,118 @@ void nowPlayingInformation(MRMediaRemoteGetNowPlayingInfoCompletion completion) 
 
 int main(int argc, const char *argv[]) {
     @autoreleasepool {
-        float ringtoneVolume;
+        switch (argc) {
+            case 2: {
+                NSString *command = [NSString stringWithUTF8String:argv[1]];
+                int volumeStep = -1;
 
-        [[AVSystemController sharedAVSystemController] getVolume:&ringtoneVolume forCategory:kAVMediaCategory];
-
-        if (argc == 2) {
-            NSString *action = [NSString stringWithUTF8String:argv[1]];
-            int volumeStep = -1;
-
-            if ([@"playpause" isEqualToString:action] || [@"toggle" isEqualToString:action]) {
-                mediaRemoteCommand(kMRTogglePlayPause);
-            }
-            else if ([@"play" isEqualToString:action]) {
-                mediaRemoteCommand(kMRPlay);
-            }
-            else if ([@"pause" isEqualToString:action]) {
-                mediaRemoteCommand(kMRPause);
-            }
-            else if ([@"stop" isEqualToString:action]) {
-                mediaRemoteCommand(kMRPause);
-            }
-            else if ([@"previous" isEqualToString:action] || [@"prev" isEqualToString:action]) {
-                mediaRemoteCommand(kMRPreviousTrack);
-            }
-            else if ([@"next" isEqualToString:action]) {
-                mediaRemoteCommand(kMRNextTrack);
-            }
-            else if ([@"back15" isEqualToString:action]) {
-                mediaRemoteCommand(kMRGoBackFifteenSeconds);
-            }
-            else if ([@"forward15" isEqualToString:action] || [@"fwd15" isEqualToString:action]) {
-                mediaRemoteCommand(kMRSkipFifteenSeconds);
-            }
-            else if ([@"voldown" isEqualToString:action] || [@"vol-" isEqualToString:action]) {
-                float currentVolume;
-                [[AVSystemController sharedAVSystemController] getVolume:&currentVolume forCategory:kAVMediaCategory];
-
-                setVolume(currentVolume - kVolumeStep);
-            }
-            else if ([@"volup" isEqualToString:action] || [@"vol+" isEqualToString:action]) {
-                float currentVolume;
-                [[AVSystemController sharedAVSystemController] getVolume:&currentVolume forCategory:kAVMediaCategory];
-
-                setVolume(currentVolume + kVolumeStep);
-            }
-            // vol0 - vol16
-            else if ((volumeStep = volMatch(action)) != -2) {
-                if (volumeStep == -1) {
-                    help(argv[0]);
-                } else {
-                    setVolume(volumeStep * kVolumeStep);
+                if ([@"playpause" isEqualToString:command] || [@"toggle" isEqualToString:command]) {
+                    mediaRemoteCommand(kMRTogglePlayPause);
                 }
+                else if ([@"play" isEqualToString:command]) {
+                    mediaRemoteCommand(kMRPlay);
+                }
+                else if ([@"pause" isEqualToString:command]) {
+                    mediaRemoteCommand(kMRPause);
+                }
+                else if ([@"stop" isEqualToString:command]) {
+                    mediaRemoteCommand(kMRPause);
+                }
+                else if ([@"previous" isEqualToString:command] || [@"prev" isEqualToString:command]) {
+                    mediaRemoteCommand(kMRPreviousTrack);
+                }
+                else if ([@"next" isEqualToString:command]) {
+                    mediaRemoteCommand(kMRNextTrack);
+                }
+                else if ([@"back15" isEqualToString:command]) {
+                    mediaRemoteCommand(kMRGoBackFifteenSeconds);
+                }
+                else if ([@"forward15" isEqualToString:command] || [@"fwd15" isEqualToString:command]) {
+                    mediaRemoteCommand(kMRSkipFifteenSeconds);
+                }
+                else if ([@"voldown" isEqualToString:command] || [@"vol-" isEqualToString:command]) {
+                    float currentVolume;
+                    [[AVSystemController sharedAVSystemController] getVolume:&currentVolume forCategory:kAVMediaCategory];
+
+                    setVolume(currentVolume - kVolumeStep);
+                }
+                else if ([@"volup" isEqualToString:command] || [@"vol+" isEqualToString:command]) {
+                    float currentVolume;
+                    [[AVSystemController sharedAVSystemController] getVolume:&currentVolume forCategory:kAVMediaCategory];
+
+                    setVolume(currentVolume + kVolumeStep);
+                }
+                // vol0 - vol16
+                else if ((volumeStep = volMatch(command)) != -2) {
+                    if (volumeStep == -1) {
+                        help(argv[0]);
+                    } else {
+                        setVolume(volumeStep * kVolumeStep);
+                    }
+                }
+
+                else if ([@"nowplaying" isEqualToString:command] || [@"np" isEqualToString:command]) {
+                    nowPlayingInformation(^(CFDictionaryRef information) {
+                        NSString *title = @"(unknown)";
+                        NSString *artist = @"(unknown)";
+
+                        if (CFDictionaryContainsKey(information, kMRMediaRemoteNowPlayingInfoTitle)) {
+                            title = CFDictionaryGetValue(information, kMRMediaRemoteNowPlayingInfoTitle);
+                        }
+                        if (CFDictionaryContainsKey(information, kMRMediaRemoteNowPlayingInfoArtist)) {
+                            artist = CFDictionaryGetValue(information, kMRMediaRemoteNowPlayingInfoArtist);
+                        }
+
+                        printf("\nTitle:  %s\nArtist: %s\n", [title UTF8String], [artist UTF8String]);
+
+                        if (CFDictionaryContainsKey(information, kMRMediaRemoteNowPlayingInfoAlbum)) {
+                            NSString *album = CFDictionaryGetValue(information, kMRMediaRemoteNowPlayingInfoAlbum);
+                            printf("\nAlbum:  %s\n", [album UTF8String]);
+                        }
+
+                        if (CFDictionaryContainsKey(information, kMRMediaRemoteNowPlayingInfoGenre)) {
+                            NSString *genre = CFDictionaryGetValue(information, kMRMediaRemoteNowPlayingInfoGenre);
+                            printf("\nGenre:  %s\n", [genre UTF8String]);
+                        }
+
+                        printf("\n");
+                    });
+
+                }
+                else if ([@"artworkuri" isEqualToString:command] || [@"awuri" isEqualToString:command]) {
+                    nowPlayingInformation(^(CFDictionaryRef information) {
+                        if (CFDictionaryContainsKey(information, kMRMediaRemoteNowPlayingInfoArtworkData)
+                            && CFDictionaryContainsKey(information, kMRMediaRemoteNowPlayingInfoArtworkMIMEType)) {
+
+                            NSString *mime = CFDictionaryGetValue(information, kMRMediaRemoteNowPlayingInfoArtworkMIMEType);
+                            NSData *data = CFDictionaryGetValue(information, kMRMediaRemoteNowPlayingInfoArtworkData);
+
+                            NSString *uri = [NSString stringWithFormat:@"data:%@;base64,%@", mime, [data base64EncodedStringWithOptions:0]];
+
+                            printf("%s\n", [uri UTF8String]);
+                        }
+                        else {
+                            printf("\nNo artwork available right now :(\n\n");
+                        }
+                    });
+                }
+
+                else if ([@"help" isEqualToString:command] || [@"-h" isEqualToString:command] || [@"--help" isEqualToString:command]) {
+                    help(argv[0]);
+                }
+                else {
+                    fprintf(stderr, "\nUnknown command: %s\n", argv[1]);
+                    help(argv[0]);
+                    return EXIT_FAILURE;
+                }
+
+                break;
             }
-            else if ([@"nowplaying" isEqualToString:action] || [@"np" isEqualToString:action]) {
-                nowPlayingInformation(^(CFDictionaryRef information) {
-                    NSString *title = @"(unknown)";
-                    NSString *artist = @"(unknown)";
-
-                    if (CFDictionaryContainsKey(information, kMRMediaRemoteNowPlayingInfoTitle)) {
-                        title = CFDictionaryGetValue(information, kMRMediaRemoteNowPlayingInfoTitle);
-                    }
-                    if (CFDictionaryContainsKey(information, kMRMediaRemoteNowPlayingInfoArtist)) {
-                        artist = CFDictionaryGetValue(information, kMRMediaRemoteNowPlayingInfoArtist);
-                    }
-
-                    printf("\nTitle:  %s\nArtist: %s\n", [title UTF8String], [artist UTF8String]);
-
-                    if (CFDictionaryContainsKey(information, kMRMediaRemoteNowPlayingInfoAlbum)) {
-                        NSString *album = CFDictionaryGetValue(information, kMRMediaRemoteNowPlayingInfoAlbum);
-                        printf("\nAlbum:  %s\n", [album UTF8String]);
-                    }
-
-                    if (CFDictionaryContainsKey(information, kMRMediaRemoteNowPlayingInfoGenre)) {
-                        NSString *genre = CFDictionaryGetValue(information, kMRMediaRemoteNowPlayingInfoGenre);
-                        printf("\nGenre:  %s\n", [genre UTF8String]);
-                    }
-
-                    printf("\n");
-                });
-
-            }
-            else if ([@"artworkuri" isEqualToString:action] || [@"awuri" isEqualToString:action]) {
-                nowPlayingInformation(^(CFDictionaryRef information) {
-                    if (CFDictionaryContainsKey(information, kMRMediaRemoteNowPlayingInfoArtworkData)
-                        && CFDictionaryContainsKey(information, kMRMediaRemoteNowPlayingInfoArtworkMIMEType)) {
-
-                        NSString *mime = CFDictionaryGetValue(information, kMRMediaRemoteNowPlayingInfoArtworkMIMEType);
-                        NSData *data = CFDictionaryGetValue(information, kMRMediaRemoteNowPlayingInfoArtworkData);
-
-                        NSString *uri = [NSString stringWithFormat:@"data:%@;base64,%@", mime, [data base64EncodedStringWithOptions:0]];
-                    
-                        printf("%s\n", [uri UTF8String]);
-                    }
-                    else {
-                        printf("\nNo artwork available right now :(\n\n");
-                    }
-                });
-            }
-            else if ([@"help" isEqualToString:action] || [@"-h" isEqualToString:action] || [@"--help" isEqualToString:action]) {
-                help(argv[0]);
-            }
-            else {
-                fprintf(stderr, "Unknown action: %s\n", argv[1]);
+            default: {
+                fprintf(stderr, "\nInvalid number of arguments: %d\n", argc - 1);
                 help(argv[0]);
                 return EXIT_FAILURE;
             }
-        } else {
-            fprintf(stderr, "Invalid number of arguments: %d", argc - 1);
-            help(argv[0]);
-            return EXIT_FAILURE;
         }
 
         return EXIT_SUCCESS;
